4fcb431a37c35123cfd4f308fb3f1cdcea96ada0
Mike Wyatt <wyatt.mike@gmail.com>
2008-09-24 05:29:27 -0230

    making the welcome/index.phtml look a little nicer

--- application/helpers/application_helper.php
+++ application/helpers/application_helper.php
@@ -3,4 +3,18 @@ class ApplicationHelper extends CoreHelper {
 	public function write_title() {
 		return implode(' - ', $this->locals->title);
 	}
+	
+	public function start_em($id = '') {
+		// <div class="em" id="whatsnew"><div class="top"><div class="right"><div class="bottom"><div class="left">
+		$divs = '<div class="em"';
+		if(!empty($id))
+			$divs .= ' id="' . $id . '"';
+		
+		$divs .= '><div class="top"><div class="right"><div class="bottom"><div class="left">';
+		return $divs;
+	}
+	
+	public function end_em() {
+		return '</div></div></div></div><div class="tl"></div><div class="tr"></div><div class="br"></div><div class="bl"></div></div>';
+	}
 }
