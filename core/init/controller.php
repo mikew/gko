14f83f5502b65eb55f75243b2360afc8f640ddfc
Mike Wyatt <mike@landmine.(none)>
2008-10-16 22:56:20 -0230

    fixed bug in determining basepath

--- core/init/controller.php
+++ core/init/controller.php
@@ -3,8 +3,10 @@ function clear_end_slash($from) {
 	return substr($from, -1) == '/' ? substr($from, 0, -1) : $from ;
 }
 
-$path = str_replace('?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']);
 define('WWW_HOME', clear_end_slash(str_replace(basename($_SERVER['PHP_SELF']), '', $_SERVER['SCRIPT_NAME'])));
+$path = str_replace('?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']);
+$path =  substr($path, strlen(WWW_HOME));
+
 $matched = $map->match($path);
 
 define('CONTROLLER', $matched['controller']);
