1c2715feb8adfa907bb1a25c21e1dbf2471f94e0
Mike Wyatt <wyatt.mike@gmail.com>
2008-11-03 04:52:09 -0330

    Controller init should now does some work with CoreMime to set things up for us

--- core/init/controller.php
+++ core/init/controller.php
@@ -4,8 +4,10 @@ $matched = CoreRouter::instance()->match(WWW_PATH);
 foreach($matched AS $key => $value) {
 	$_GET[$key] = $value;
 }
-// if(empty($_GET['format']))
-// 	$_GET['format'] = 'html';
+if(empty($_GET['format']))
+	$_GET['format'] = 'html';
+
+CoreMime::set($_GET['format']);
 
 define('CONTROLLER', str_replace('/', '_', $matched['controller']));
 define('ACTION', $matched['action']);
