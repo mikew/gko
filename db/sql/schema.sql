a3fe9ee5bdd4056d06a4e09591cd9c4c20b25463
Mike Wyatt <wyatt.mike@gmail.com>
2008-11-10 06:30:39 -0330

    updated schema/fixtures

--- db/sql/schema.sql
+++ db/sql/schema.sql
@@ -1,4 +1,6 @@
+CREATE TABLE authors (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), handle VARCHAR(255), password VARCHAR(255), created_at DATETIME, updated_at DATETIME, slug VARCHAR(255));
 CREATE TABLE games (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255), genre VARCHAR(255), online INTEGER, multiplayer INTEGER, blurb TEXT, howto TEXT, created_at DATETIME, updated_at DATETIME, slug VARCHAR(255));
-CREATE TABLE news (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255), body TEXT, created_at DATETIME, updated_at DATETIME, slug VARCHAR(255));
+CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255), body TEXT, created_at DATETIME, updated_at DATETIME, slug VARCHAR(255));
+CREATE INDEX authors_slug_idx ON authors (slug);
 CREATE INDEX games_slug_idx ON games (slug);
-CREATE INDEX news_slug_idx ON news (slug);
+CREATE INDEX posts_slug_idx ON posts (slug);
