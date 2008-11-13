b3bb8cb6ddccb4268db0e65e334a1f64b560e575
Mike Wyatt <wyatt.mike@gmail.com>
2008-11-13 08:52:15 -0330

    still fixing things from the news->posts migration

--- config/routes.php
+++ config/routes.php
@@ -2,9 +2,15 @@
 $map->resource('admin/game', 'admin/games');
 $map->resource('admin/post', 'admin/posts');
 $map->connect('root', '/', array('controller'=>'welcome', 'action'=>'index'));
+
 $map->connect('/news/archive/:key', array(
-	'controller' => 'news',
+	'controller' => 'posts',
 	'action' => 'show'
 ));
+$map->connect('/news', array(
+	'controller' => 'posts',
+	'action' => 'index'
+));
+
 $map->connect('rss', '/rss', array('controller' => 'posts', 'action' => 'index', 'format' => 'rss'));
 $map->connect(':controller/:action/:id.:format', array('format' => 'html'));
