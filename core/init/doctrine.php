c901ac205d1c5a4cc4c83ff8c3131845a1c39bbf
Mike Wyatt <wyatt.mike@gmail.com>
2008-11-25 01:05:54 -0330

    turn on doctrine validations (next step is making them smarter)

--- core/init/doctrine.php
+++ core/init/doctrine.php
@@ -4,6 +4,8 @@ $config = Doctrine_Parser::load(File::join(FW_HOME, 'config', 'database.yml'), '
 
 // TODO: remove ['default'] use
 Doctrine_Manager::connection($config['default']['adapter'] . ':' . FW_HOME . '/' . $config['default']['database'], 'gko');
+
 Doctrine_Manager::getInstance()->setAttribute('model_loading', 'conservative');
+Doctrine_Manager::getInstance()->setAttribute('validate', 'constraints');
 
 unset($config);
