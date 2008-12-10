ff6612ab13c2dcfc9d9e59e1a9df781cd153767b
Mike Wyatt <mike@mike-wyatts-macbook.local>
2008-12-10 02:44:17 -0330

    Actually, Author recknogition is needed to trule finish Posts

---
+++ application/controllers/admin/authors_controller.php
@@ -0,0 +1,19 @@
+<?php
+class Admin_AuthorsController extends AdminController {
+	public static function setup($self) {
+		array_push($self->title, 'Authors');
+	}
+	
+	public function index() {
+		$this->authors = Doctrine_query::create()->from('Authors a')->execute();
+	}
+	
+	public function _new() {
+		$this->author = new Authors();
+	}
+	
+	public function edit() {
+		// $this->post = Doctrine_Query::create()->from('Posts')->where('key = ?', $_GET['id'])->fetchOne();
+		$this->author = Doctrine_Query::create()->from('Authors')->where('handle = ?', $_GET['id'])->fetchOne();
+	}
+}
--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,8 @@
 <?php
 $map->resource('admin/game', 'admin/games');
 $map->resource('admin/post', 'admin/posts');
+$map->resource('admin/author', 'admin/authors');
+
 $map->connect('root', '/', array('controller'=>'welcome', 'action'=>'index'));
 
 $map->connect('/news/archive/:key', array(
