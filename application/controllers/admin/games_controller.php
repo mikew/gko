8a8384060c41abc781ba412c2ec69dd1e0438125
Mike Wyatt <mike@mike-wyatts-macbook.local>
2008-12-10 02:43:34 -0330

    Admin_GamesController now makes sure you know where you are

--- application/controllers/admin/games_controller.php
+++ application/controllers/admin/games_controller.php
@@ -1,5 +1,9 @@
 <?php
 class Admin_GamesController extends AdminController {
+	public static function setup($self) {
+		array_push($self->title, 'Games');
+	}
+	
 	public function index() {
 		$this->games = Doctrine_query::create()->from('Games g')->execute();
 	}
