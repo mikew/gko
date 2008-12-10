78b809194192dd1cebf66e3f08d618ec850a5319
Mike Wyatt <mike@mike-wyatts-macbook.local>
2008-12-10 02:37:29 -0330

    CoreFormHelper::form_tag_for now supports a reference variable and will give you a form builder
    example:
      <?= $context->form_tag_for($context->post, null, &$f) ?>
      <?= $f->text_field('title') ?>
      // is the same as
      <?= $context->text_field('post', 'title') ?>

--- core/helpers/core_form_helper.php
+++ core/helpers/core_form_helper.php
@@ -15,10 +15,10 @@ class CoreFormHelper {
 		return $html;
 	}
 	
-	public static function form_tag_for($parts, $options = array()) {
+	public static function form_tag_for($parts, $options = array(), &$object_reference = false) {
 		if(!is_array($parts))
 			$parts = array($parts);
-		
+			
 		$object = array_pop($parts);
 		
 		$key = 'id';
@@ -27,12 +27,14 @@ class CoreFormHelper {
 			$object = array_pop($parts);
 		}
 		
+		if($object_reference !== false)
+			$object_reference = new CoreFormBuilder(get_class($object));
+		
 		$action = $object->exists() ? 'update' : 'create';
 		$class = $object->exists() ? Inflector::singularize(get_class($object)) : get_class($object);
 		array_push($parts, $class);
 		
 		$path_name = $action . '_' . implode('/', $parts);
-		
 		$path_name = String::lowercase($path_name);
 		
 		return CoreHelper::instance()->form_tag(array($path_name, array('id' => $object->{$key})), $object->exists() ? 'put' : 'post', $options);
@@ -40,6 +42,16 @@ class CoreFormHelper {
 	
 	public static function end_form_tag() {
 		return '</form>';
+	}
+
+	public static function label($object, $key, $text = '', $attributes = array()) {
+		// print_r(func_get_args());
+		if(empty($text))
+			$text = Inflector::titleize($key);
+
+		return CoreHelper::instance()->tag('label', CoreHelper::instance()->merge_attributes($attributes, array(
+			'for' => self::interpret_pair($object, $key, 'id')
+		)), $text);
 	}
 	
 	public static function text_field($object, $key, $attributes = array()) {
@@ -63,12 +75,48 @@ class CoreFormHelper {
 		return CoreHelper::instance()->tag('textarea', $attributes, self::interpret_pair($object, $key));
 	}
 	
+	public static function password_field($object, $key, $attributes = array()) {
+		return CoreHelper::instance()->simple_tag('input', CoreHelper::instance()->merge_attributes($attributes, array(
+			'type' => 'password',
+			'name' => self::interpret_pair($object, $key, 'name'),
+			'id' => self::interpret_pair($object, $key, 'id'),
+			'value' => self::interpret_pair($object, $key)
+		)));
+	}
+	
+	public static function confirm_password_field($object, $key, $attributes = array()) {
+		return CoreHelper::instance()->simple_tag('input', CoreHelper::instance()->merge_attributes($attributes, array(
+			'type' => 'password',
+			'name' => self::interpret_pair($object, $key . '_confirmation', 'name'),
+			'id' => self::interpret_pair($object, $key . '_confirmation', 'id'),
+			'value' => self::interpret_pair($object, $key)
+		)));
+	}
+	
 	public static function hidden_field($name, $value) {
 		return CoreHelper::instance()->simple_tag('input', array(
 			'type' => 'hidden',
 			'name' => $name,
 			'value' => $value
 		));
+	}
+	
+	public static function check_box($object, $key) {
+		$value = self::interpret_pair($object, $key);
+		$attributes = array(
+			'id' => self::interpret_pair($object, $key, 'id'),
+			'name' => self::interpret_pair($object, $key, 'name'),
+			'type' => 'checkbox',
+			'value' => '1'
+		);
+		
+		if(!empty($test))
+			$attributes = array('checked' => true);
+			
+		$checkbox = CoreHelper::instance()->simple_tag('input', $attributes);
+		$hidden = self::hidden_field(self::interpret_pair($object, $key, 'name'), 0);
+		
+		return $checkbox . $hidden;
 	}
 	
 	public static function submit_button($value = 'Continue', $options = array()) {
@@ -94,18 +142,21 @@ class CoreFormHelper {
 	}
 	
 	public static function errors_for($object) {
-		$object = CoreContext::instance()->{$object};
+		if(is_string($object))
+			$object = CoreContext::instance()->{$object};
+		
 		$stack = $object->errorStack();
+		if($stack->count() > 0) {
+			$contents = CoreHelper::instance()->tag('h1', '', CoreHelper::pluralize($stack->count(), 'error') . ' occurred');
+			$contents .= '<ul>';
+			foreach($stack AS $key => $value) {
+				// echo "{$key} => " . var_dump($value) . '<br />';
+				$contents .= CoreHelper::instance()->tag('li', '', implode(', ', $value) . ' on ' . $key);
+			}
+			$contents .= '</ul>';
 		
-		$contents = CoreHelper::instance()->tag('h1', '', CoreHelper::pluralize($stack->count(), 'error') . ' occurred');
-		$contents .= '<ul>';
-		foreach($stack AS $key => $value) {
-			// echo "{$key} => " . var_dump($value) . '<br />';
-			$contents .= CoreHelper::instance()->tag('li', '', implode(', ', $value) . ' on ' . $key);
+			return CoreHelper::instance()->tag('div', array('class' => 'errors'), $contents);
 		}
-		$contents .= '</ul>';
-		
-		return CoreHelper::instance()->tag('div', array('class' => 'errors'), $contents);
 	}
 	
 	private static function interpret_pair($object, $key, $as = 'data') {
@@ -121,5 +172,18 @@ class CoreFormHelper {
 				return CoreContext::get($object)->{$key};
 				break;
 		}
+	}
+}
+
+class CoreFormBuilder {
+	private $object_name;
+	public function __construct($name) {
+		$this->object_name = String::lowercase(Inflector::singularize($name));
+	}
+	
+	public function __call($method, $arguments) {
+		array_unshift($arguments, $this->object_name);
+		// var_dump($arguments);
+		return call_user_func_array(array('CoreFormHelper', $method), $arguments);
 	}
 }
