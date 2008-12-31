fa16998eb069203ebc100efeaf96320e26046464
Mike Wyatt <mike@mike-wyatts-macbook.local>
2008-12-31 18:29:29 -0330

    Use new features in Admin_PostsController, new CoreFormHelpers in admin/posts/post_form partial, and a few tweaks to the CSS

--- application/controllers/admin/posts_controller.php
+++ application/controllers/admin/posts_controller.php
@@ -1,10 +1,15 @@
 <?php
 class Admin_PostsController extends AdminController {
+	public static function setup($self) {
+		array_push($self->title, 'Posts');
+	}
+	
 	public function index() {
 		$this->posts = Doctrine_Query::create()->from('Posts p')->execute();
 	}
 	
 	public function _new() {
+		array_push($this->title, 'New');
 		$this->post = new Posts();
 	}
 	
@@ -13,18 +18,25 @@ class Admin_PostsController extends AdminController {
 		$this->post->merge($_POST['post']);
 		if(!$this->post->trySave()) {
 			$this->action_to_render = '_new';
+			return;
 		}
-		// print_r($this->post->errorStack());
+		
+		$this->flash->success = '"' . $this->post->title . '" was created!';
+		
+		CoreRouter::redirect_to('admin/posts');
 	}
 	
 	public function edit() {
 		$this->post = Doctrine_Query::create()->from('Posts')->where('key = ?', $_GET['id'])->fetchOne();
+		$this->breadcrumbs[$this->post->key] = $this->post->title;
 	}
 	
 	public function update() {
 		$this->post = Doctrine_Query::create()->from('Posts')->where('key = ?', $_GET['id'])->fetchOne();
 		$this->post->merge($_POST['post']);
 		$this->post->save();
+		
+		$this->flash->success = '"' . $this->post->title . '" was updated!';
 		
 		CoreRouter::redirect_to('admin/posts');
 	}
